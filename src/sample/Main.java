56cf4c9fdd697a3b4a85302dc0bbbf2c73ceb50a
Sebastian M <sebi.markus@googlemail.com>
2019-12-10 15:39:57 +0100

    asda

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,12 +9,32 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void anwendug(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("anwendung.fxml"));
+        primaryStage.setTitle("Benutzerverwaltung");
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.show();
+    }
+
+    public void login(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Benutzerverwaltung");
         primaryStage.setScene(new Scene(root, 400, 250));
         primaryStage.show();
+    }
+
+    public void anmeldung(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("anmeldung.fxml"));
+        primaryStage.setTitle("Benutzerverwaltung");
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.show();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //anwendug(primaryStage);
+        //login(primaryStage);
+        anmeldung(primaryStage);
     }
 
     public static void main(String[] args) {
